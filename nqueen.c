d084cac908078f3515caa72d5b0c0bc17d2b9e3d
Itsuki Dhanu <itsuki@example.com>
2026-05-31 21:35:38 +0530

    Refactor N-Queens implementation: streamline code structure and improve readability

--- nqueen.c
+++ nqueen.c
@@ -1,56 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int x[20], n;
 
-// Check whether queen can be placed
 int safe(int row, int col)
 {
-    for (int i = 1; i < row; i++)
-    {
-        if (x[i] == col || abs(x[i] - col) == abs(i - row))
+    for(int i = 1; i < row; i++)
+        if(x[i] == col || (i - x[i]) == (row - col) || (i + x[i]) == (row + col))
             return 0;
-    }
     return 1;
 }
 
-// Solve N Queen
 void queen(int row)
 {
-    if (row > n)
+    if(row > n)
     {
         printf("\nSolution:\n");
-
-        for (int i = 1; i <= n; i++)
+        for(int i = 1; i <= n; i++)
         {
-            for (int j = 1; j <= n; j++)
-            {
-                if (x[i] == j)
+            for(int j = 1; j <= n; j++)
+                if(x[i] == j)
                     printf(" Q ");
                 else
                     printf(" . ");
-            }
             printf("\n");
         }
         return;
     }
 
-    for (int col = 1; col <= n; col++)
-    {
-        if (safe(row, col))
+    for(int col = 1; col <= n; col++)
+        if(safe(row, col))
         {
             x[row] = col;
             queen(row + 1);
         }
-    }
 }
 
 int main()
 {
     printf("Enter value of N: ");
     scanf("%d", &n);
-
     queen(1);
-
     return 0;
 }
